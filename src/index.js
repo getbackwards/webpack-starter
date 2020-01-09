e96fbed3e520fb833737d60fcd2f372dd9f899e6
Your Name <getbackwards@gmail.com>
2020-01-10 11:01:34 +1300

    adding footer ts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { Header } from './app/header';
+import { Footer } from './app/footer';
 
 let header = new Header();
 let firstHeading = header.getFirstHeading();
 console.log(firstHeading);
 
+let footer = new Footer();
+let footerText = footer.getFooterText();
+
+// CONSOLE LOGS
 console.log('This is index JS');
+console.log(footerText);
