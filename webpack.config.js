9ecb87a15cdbc922899c8e8fb3b97fe0795d4b53
Your Name <getbackwards@gmail.com>
2020-01-27 10:18:47 +1300

    adding autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,8 @@ module.exports = {
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'sass-loader'
+                    'sass-loader',
+                    'postcss-loader'
                 ]
             }
         ]
