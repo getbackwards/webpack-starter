74774e6d4a0832c26c6f4ab614a766cb62a94baf
Your Name <getbackwards@gmail.com>
2020-01-27 12:05:44 +1300

    adding copy-webpack

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import '../src/style.css';
 import './styles/scss/main.scss';
 
 // images
-import webpackgif from './assets/images/webpack.gif';
+// This line is not need once copy-webpack is being used
+// https://github.com/webpack-contrib/copy-webpack-plugin
 // import webpackgif from './assets/images/webpack.gif';
 
 let header = new Header();
@@ -17,7 +18,9 @@ let footer = new Footer();
 let footerText = footer.getFooterText();
 
 // setting the source of img
-document.getElementById('webpack-gif').setAttribute('src', webpackgif);
+// This line is not need once copy-webpack is being used
+// https://github.com/webpack-contrib/copy-webpack-plugin
+// document.getElementById('webpack-gif').setAttribute('src', webpackgif);
 
 // CONSOLE LOGS
 console.log('This is index JS');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -23,7 +24,11 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: 'style.css'
-        })
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/assets/images',
+            to: 'assets/images'
+        }])
     ],
     module: {
         rules: [
