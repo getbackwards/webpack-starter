4f16f26c3106c030997f7575e9c1f85ec7b464d5
Your Name <getbackwards@gmail.com>
2020-02-04 14:56:27 +1300

    adding common dev and prod config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,7 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const environment = (process.env.NODE_ENV || 'development').trim();
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack 4 Starter',
-            template: './src/index.html',
-            inject: true,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: false
-            }
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style.css'
-        }),
-        new CopyWebpackPlugin([{
-            from: './src/assets/images',
-            to: 'assets/images'
-        }])
-    ],
-    module: {
-        rules: [
-            {
-                test: [/.js$|.ts$/],
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/typescript'
-                        ]
-                    }
-                }
-            },
-            {
-                test: [/.css$|.scss$/],
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                    'postcss-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'assets/images'
-                    }
-                }
-            }
-        ]
-    }
-};
+if (environment === 'development') {
+    module.exports = require('./config/webpack.dev.config.js');
+} else {
+    module.exports = require('./config/webpack.prod.config.js');
+}
