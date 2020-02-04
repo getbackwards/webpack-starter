28165bcf9c56eab46d375cedf7a7762c037fbe85
Your Name <getbackwards@gmail.com>
2020-02-04 18:21:56 +1300

    adding hashing to generated files

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -7,8 +7,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].[chunkhash].js',
+
     },
     resolve: {
         extensions: ['.ts', '.js']
@@ -24,7 +25,7 @@ module.exports = {
             }
         }),
         new MiniCssExtractPlugin({
-            filename: 'style.css'
+            filename: 'style.[chunkhash].css'
         }),
         new CopyWebpackPlugin([{
             from: './src/assets/images',
